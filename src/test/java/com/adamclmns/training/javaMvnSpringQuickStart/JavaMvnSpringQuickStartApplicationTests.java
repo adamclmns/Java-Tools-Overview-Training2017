f56d5326c0d1fe8db0c138fe8a1b0f01f5451d71
Adam Clemons <adam@adamclmns.com>
2017-06-05 08:47:31 -0500

    Adding Readme, POM from spring Initializr minus MVNW
    Will build as cross-platform.

---
+++ src/test/java/com/adamclmns/training/javaMvnSpringQuickStart/JavaMvnSpringQuickStartApplicationTests.java
@@ -0,0 +1,16 @@
+package com.adamclmns.training.javaMvnSpringQuickStart;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavaMvnSpringQuickStartApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
