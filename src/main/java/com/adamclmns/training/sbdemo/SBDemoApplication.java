284a3cca7632de527df9e5573a7b05f7dd73d620
adamclmns <adam@adamclmns.com>
2017-07-07 13:35:26 -0500

    This is now a deployable WAR file. Tested with Tomcat7 on CentOS7 Pom also supports Tomcat7:deploy goal if you have the appropriate server configured in your MAVEN_HOME\conf\settings.xml

--- src/main/java/com/adamclmns/training/sbdemo/SBDemoApplication.java
+++ src/main/java/com/adamclmns/training/sbdemo/SBDemoApplication.java
@@ -10,16 +10,24 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import com.adamclmns.training.sbdemo.repo.CustomerRepo;
 import com.adamclmns.training.sbdemo.repo.ProductRepo;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SBDemoApplication {
+public class SBDemoApplication extends SpringBootServletInitializer{
 
     private static final Logger log = LoggerFactory.getLogger(SBDemoApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(SBDemoApplication.class, args);
     }
-
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+        return application.sources(applicationClass);
+    }
+    
+    private static Class<SBDemoApplication> applicationClass = SBDemoApplication.class;
     @Bean
     public CommandLineRunner loadData(CustomerRepo customeRepo, ProductRepo productRepo) {
         return (args) -> {
